9763642cb24b3be092e73ea40be7939313eb0bf6
tarzemanovrr <tarzemanovrr@63pokupki.ru>
2023-12-28 15:20:15 +0400

    in work

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TheHeader/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from "./components/TheHeader"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TheHeader,
   }
 }
 </script>
@@ -24,5 +23,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+  background: #0A1F38;
 }
 </style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="header">
+    <img src="../assets/logo.svg" />
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'TheHeader',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+</style>
